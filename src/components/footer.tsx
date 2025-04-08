23bbf018e04a2f37c64790bd75c6c7971ebc99bb
Heather Davies <heather@Heathers-MacBook-Pro-2.local>
2025-04-08 00:05:53 -0400

    asdfasdfasdf

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="w-full  mx-auto bg-transparent py-4 mt-auto">
-      <div className="max-w-7xl mx-auto text-sm text-white/80">
+      <div className="max-w-7xl mx-auto text-sm text-gray-700 dark:text-white/80">
         <p>&copy; {new Date().getFullYear()} Heather Davies. All rights reserved.</p>
       </div>
     </footer>
