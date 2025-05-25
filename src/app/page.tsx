fe92ef7c1e823fecc3aa7c7684367b9cac5b0bd4
Heather Davies <heather@Heathers-MacBook-Pro-2.local>
2025-05-24 20:45:41 -0400

    asf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function ProjectsPage() {
       <Navigation />
 
       {/* Hero Section */}
-      <section className="flex items-center justify-center px-4 border-b border-gray-300/80 dark:border-white/10 h-[1080px]">
+      <section className="flex items-center justify-center px-4 border-b border-gray-300/80 dark:border-white/10 h-[800px]">
         <div className="flex items-center justify-center relative w-full h-full">
           <div className="absolute inset-0 -z-10 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
           
