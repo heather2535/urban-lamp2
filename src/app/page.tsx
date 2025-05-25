352aa7ab7d9be1aea9667ab2d0abc8cb3a07716f
Heather Davies <heather@Heathers-MacBook-Pro-2.local>
2025-05-24 20:59:23 -0400

    asdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function ProjectsPage() {
   }
 
   return (
-    <div className="group min-h-screen bg-background fade-in bg-gradient-to-r from-white via-[#e6f0ff] to-[#f0e6ff] dark:from-black dark:via-[#1a2a3a] dark:to-[#6a4e92]">
+    <div className="group min-h-screen bg-background fade-in bg-gradient-to-r from-white via-[#e6f0ff] to-[#f0e6ff] dark:from-black dark:via-[#1a2a3a] dark:to-[#2a3a4a]">
       <AuraCursor />
       <Navigation />
 
